0e7eb987774201cbe38d95d2aa1ced863eba06d0
codingaplus <133897645+codingaplus@users.noreply.github.com>
2023-06-02 01:40:13 +0900

    Add files via upload

--- controllers/playCtrl.js
+++ controllers/playCtrl.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const connection=require('../dbconfig');
 const express = require('express');
 const app = express(); //새로운 객체 만들어주는거
@@ -7,11 +8,19 @@ const playCtrl={
     getplay : async(req, res, next)=>{
         var query = (url.parse(req.url, true).query.query);
         connection.query(`UPDATE music SET hit = hit +1 WHERE title='${query}'`);
-        connection.query(`SELECT * FROM music WHERE title = '${query}'`,(error,rows)=>{
-            if(error) throw error;
-            console.log(rows);
-            res.render('../pages/play.ejs', {data: rows[0]});
+        connection.query(`SELECT *FROM music ORDER BY hit DESC`,(error,ROW)=>{
+            connection.query(`SELECT * FROM music WHERE title = '${query}'`,(error,rows)=>{
+                if(error) throw error;
+                console.log("여기서 말할거임");
+                console.log(ROW[0]);
+                console.log("여기까지 말할거임");
+                res.render('../pages/play.ejs', {data: rows[0],ROWS:ROW});
+            });
         });
+        // connection.query(`SELECT * FROM music WHERE title = '${query}'`,(error,rows)=>{
+
+        //     res.render('../pages/play.ejs', {data: rows[0],ROWS:rows});
+        // });
     }
 }
 
